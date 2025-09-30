92aa2c73c79e409f4ff1f1e68f3bcbb05ce5f842
“Elisa <elisareyes355@gmail.com>
2025-09-29 23:19:51 -0500

    Updates oss.c file

--- oss.c
+++ oss.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
 
 int shm_key;
 int shm_id;
@@ -16,10 +19,13 @@ typedef struct {
 
 typedef struct {
     int occupied;
-    pid_t pid; 
+    pid_t pid;
     int startSeconds;
-    int startNano; 
-} SystemClock;
+    int startNano;
+} PCB;
+
+PCB processTable[20];
+Clock *clock;
 
 void print_help() {
     printf("How to use: oss [-h] [-s simul] [-t timelimitForChildren] [-i intervalInMsToLaunchChildren]\n");
@@ -33,8 +39,10 @@ void print_help() {
 int main(int argc, char *argv[]) {
     int totalChildren = 0;
     int maxSimul = 0;
-    int timeLimit = 0;
-    int interval = 0;
+    float timeLimit = 0.0;
+    float interval = 0.0;
+    int childrenLaunched = 0;
+    int childrenTerminated = 0;
     char opt;
     const char optstring[] = "hs:t:i:";
 
@@ -61,12 +69,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("OSS starting, PID: %d PPID: %d\n", getpid(), getppid());
-    printf("Called with:\n");
-    printf("-n %d\n", totalChildren);
-    printf("-s %d\n", maxSimul);
-    printf("-t %d\n", timeLimit);
-    printf("-i %d\n", interval);
+    //initialize the process table
+    for (int i = 0; i < 20; i++) {
+        processTable[i].occupied = 0;
+        processTable[i].pid = 0;
+        processTable[i].startSeconds = 0;
+        processTable[i].startNano = 0;
+    }
 
     //checks for required parameters
     if (maxSimul <= 0 || timeLimit <= 0 || interval <= 0) {
@@ -96,14 +105,134 @@ int main(int argc, char *argv[]) {
     clock->seconds = 0;
     clock->nanoseconds = 0;
 
-    //launch worker processes
-    pid_t child_pid = fork();
-    if (child_pid == 0) {
-        char *args[] = {"./worker"};
-        execlp(args[0], args[0], (char *) 0);
+    printf("OSS starting, PID: %d PPID: %d\n", getpid(), getppid());
+    printf("Called with:\n");
+    printf("-n %d\n", totalChildren);
+    printf("-s %d\n", maxSimul);
+    printf("-t %2f\n", timeLimit);
+    printf("-i %3f\n", interval);
+
+    // Convert time limit to seconds and nanoseconds
+    int timeLimitSec = (int)timeLimit;
+    int timeLimitNano = (int)((timeLimit - timeLimitSec) * 1000000000);
+    
+    int lastPrintTime = 0;
+    int lastLaunchTime = 0;
+
+    while (childrenLaunched < totalChildren || childrenTerminated < totalChildren) {
+        //advance clock by 1 million nanoseconds
+        clock->nanoseconds += 1000000;
+
+        if (clock->nanoseconds >= 1000000000) {
+            clock->seconds++;
+            clock->nanoseconds -= 1000000000;
+        }
+
+        //check if time limit reached
+        if (clock->seconds > timeLimitSec || 
+           (clock->seconds == timeLimitSec && clock->nanoseconds > timeLimitNano)) {
+            printf("Time limit reached. Terminating.\n");
+            break;
+        }
+
+        //print process table every half second
+        if (clock->seconds >= lastPrintTime + 0.5) {
+            printf("OSS PID:%d SysClockS: %d, SysClockNano: %d\n", getpid(), clock->seconds, clock->nanoseconds);
+            printf("Process Table:\n");
+            printf("Entry\tOccupied\tPID\tStartS\tStartN\n");
+
+            for (int i = 0; i < 20; i++) {
+                printf("%d\t%d\t%d\t%d\t%d\n", 
+                    i, processTable[i].occupied, processTable[i].pid, processTable[i].startSeconds, processTable[i].startNano);
+            }
+            printf("\n");
+            lastPrintTime = clock->seconds;
+        }
+        
+        //check for terminated children
+        int status;
+        pid_t pid = waitpid(-1, &status, WNOHANG);
+        while (pid > 0) {
+            //find and free process table entry
+            for (int i = 0; i < 20; i++) {
+                if (processTable[i].occupied && processTable[i].pid == pid) {
+                    processTable[i].occupied = 0;
+                    processTable[i].pid = 0;
+                    processTable[i].startSeconds = 0;
+                    processTable[i].startNano = 0;
+                    childrenTerminated++;
+                    printf("Child process %d terminated.\n", pid);
+                    break;
+                }
+            }
+        }
+
+        //launch new children when conditions met
+        if (childrenLaunched < totalChildren) {
+            int currentChildren = childrenLaunched - childrenTerminated;
+            int timeSinceLastLaunchSec = (clock->seconds - lastLaunchTime) * 1000000000 + (clock->nanoseconds - lastLaunchTime);
+
+            if (currentChildren < maxSimul && 
+                timeSinceLastLaunchSec >= interval * 1000000000) {
+                //find free entry in table entry
+                for (int i = 0; i < 20; i++) {
+                    if (!processTable[i].occupied) {
+                        break;
+                    }
+                }
+
+                pid_t child_pid = fork();
+                if (child_pid == 0) {
+                    char secStr[20];
+                    char nanoStr[20];
+                    snprintf(secStr, sizeof(secStr), "%d", 1); //1 second
+                    snprintf(nanoStr, sizeof(nanoStr), "%d", 0); //0 nanoseconds
+                    execl("./worker", "worker", secStr, nanoStr, NULL);
+                } else if (child_pid > 0) {
+                    //parent process
+                    processTable[i].occupied = 1;
+                    processTable[i].pid = child_pid;
+                    processTable[i].startSeconds = clock->seconds;
+                    processTable[i].startNano = clock->nanoseconds;
+                    childrenLaunched++;
+                    lastLaunchTime = clock->seconds;
+                    printf("Launched child process %d\n", child_pid);
+                } else {
+                    fprintf(stderr, "Fork failed\n");
+                }
+                lastLaunchTime = clock->seconds * 1000000000 + clock->nanoseconds;
+            }
+        }
     }
 
-    wait(0);
+    while (childrenTerminated < childrenLaunched) {
+        int status;
+        pid_t pid = waitpid(-1, &status, WNOHANG);
+        if (pid > 0) {
+            //find and free process table entry
+            for (int i = 0; i < 20; i++) {
+                if (processTable[i].occupied && processTable[i].pid == pid) {
+                    processTable[i].occupied = 0;
+                    processTable[i].pid = 0;
+                    processTable[i].startSeconds = 0;
+                    processTable[i].startNano = 0;
+                    childrenTerminated++;
+                    printf("Child process %d terminated.\n", pid);
+                    break;
+                }
+            }
+        }
+
+        clock->nanoseconds += 10000;
+        if (clock->nanoseconds >= 1000000000) {
+            clock->seconds++;
+            clock->nanoseconds -= 1000000000;
+        }
+    }
+
+    printf("OSS PID:%d Terminating\n", getpid());
+    printf("%d workers were launched and terminated\n", childrenLaunched);
+    printf("Workers ran for a combined time of %d seconds %d nanoseconds\n", clock->seconds, clock->nanoseconds);
 
     shmdt(clock);
     clock = 0;
